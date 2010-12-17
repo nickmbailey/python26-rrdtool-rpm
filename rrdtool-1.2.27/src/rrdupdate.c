5a326e04a83d215e02aa7ed1ce789c5c46738175
Nick Bailey <nickmbailey@rackspace.com>
2010-12-17 20:44:53 +0000

    initial commit

---
+++ rrdtool-1.2.27/src/rrdupdate.c
@@ -0,0 +1,35 @@
+/*****************************************************************************
+ * RRDtool 1.2.27  Copyright by Tobi Oetiker, 1997-2008
+ *****************************************************************************
+ * rrdupdate.c  Main program for the (standalone) rrdupdate utility
+ *****************************************************************************
+ * $Id: rrdupdate.c 1286 2008-02-17 10:08:10Z oetiker $
+ *****************************************************************************/
+
+#if defined(_WIN32) && !defined(__CYGWIN__) && !defined(__CYGWIN32__) && !defined(HAVE_CONFIG_H)
+#include "../win32/config.h"
+#else
+#ifdef HAVE_CONFIG_H
+#include "../rrd_config.h"
+#endif
+#endif
+
+#include "rrd.h"
+
+int 
+main(int argc, char **argv){
+        rrd_update(argc,argv);
+        if (rrd_test_error()) {
+                printf("RRDtool " PACKAGE_VERSION "  Copyright by Tobi Oetiker, 1997-2008\n\n"
+                        "Usage: rrdupdate filename\n"
+                        "\t\t\t[--template|-t ds-name:ds-name:...]\n"
+                        "\t\t\ttime|N:value[:value...]\n\n"
+                        "\t\t\tat-time@value[:value...]\n\n"
+                        "\t\t\t[ time:value[:value...] ..]\n\n");
+                                   
+                printf("ERROR: %s\n",rrd_get_error());
+                rrd_clear_error();                                                            
+                return 1;
+        }
+        return 0;
+}
